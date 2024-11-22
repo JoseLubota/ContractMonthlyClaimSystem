c585cdec039d8bd944fbdd39ee2cc33034f16167
Jose Lubota <133894595+JoseLubota@users.noreply.github.com>
2024-11-22 20:55:12 +0200

    Create AppDbContext.cs
    ADbContext added inside data folder

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContractMonthlyClaimSystem.Models;
+using System.Reflection.Emit;
+namespace ContractMonthlyClaimSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<ClaimModel> Claims { get; set; }
+        public DbSet<cmcs_userTBLModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClaimModel>().ToTable("claimTBL");
+            modelBuilder.Entity<cmcs_userTBLModel>().ToTable("cmcs_userTBL");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:jkingserver.database.windows.net,1433;Initial Catalog=test;Persist Security Info=False;User ID=Jking;Password=Fr@ney04;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
