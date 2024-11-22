29da00e94122c3c84ea953600c86e1411974c591
Jose Lubota <133894595+JoseLubota@users.noreply.github.com>
2024-11-22 21:10:35 +0200

    Update Services
    Added Claim and User Services

---
+++ Service/ClaimService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace ContractMonthlyClaimSystem.Service;
+using ContractMonthlyClaimSystem.Data;
+using ContractMonthlyClaimSystem.Models;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Extensions.Azure;
+using System.Reflection.Metadata;
+using Document = iTextSharp.text.Document;
+
+public interface IClaimService
+{
+    List<ClaimModel> GetClaimsByLecturer(int lecturerID);
+    byte[] GenerateClaimReport(List<ClaimModel> claims);
+}
+public class ClaimService : IClaimService
+{
+    private readonly AppDbContext _context;
+    public ClaimService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<ClaimModel> GetClaimsByLecturer(int lecturerID)
+    {
+        return _context.Claims.Where(c => c.LECTURER_ID == lecturerID).ToList();
+    }
+
+    public byte[] GenerateClaimReport(List<ClaimModel> claims)
+    {
+        using (var stream = new MemoryStream())
+        {
+            var document = new Document();
+            PdfWriter.GetInstance(document, stream);
+            document.Open();
+            document.Add(new Paragraph("                                        Lecturer Claim Report" + "\n\n\n"));
+            foreach (var claim in claims)
+            {
+                document.Add(new Paragraph(
+                    $"Claim ID: {claim.CLAIM_ID}" + "\n" +
+                    $"Lecturer ID: {claim.LECTURER_ID}" + "\n" +
+                    $"Hours Worked: {claim.HOURS_WORKED}" + "\n" +
+                    $"Hours Worked: {claim.HOURS_WORKED}" + "\n" +
+                    $"Hourly Rate: {claim.HOURLY_RATE}" + "\n" +
+                    $"Final Payment: {Convert.ToDouble(claim.HOURS_WORKED) * Convert.ToDouble(claim.HOURLY_RATE)}" + "\n\n\n\n\n" +
+                    $"Additiona Notes: {claim.NOTES}"));
+            }
+            document.Close();
+            return stream.ToArray();
+        }
+    }
+
+}
+
+
---
+++ Service/UserService.cs
@@ -0,0 +1,37 @@
+﻿using ContractMonthlyClaimSystem.Data;
+using ContractMonthlyClaimSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractMonthlyClaimSystem.Service
+{
+    public interface IUserService
+    {
+        cmcs_userTBLModel GetUserById(int userId);
+        void UpdateUser(cmcs_userTBLModel user);
+        //  Task<int?> GetLastUserIDAsync();
+        //  Task<int?> Insert_User(cmcs_userTBL user);
+
+    }
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public cmcs_userTBLModel GetUserById(int userId)
+        {
+            return _context.Users.Find(userId);
+        }
+
+        public void UpdateUser(cmcs_userTBLModel user)
+        {
+            _context.Users.Update(user);
+            _context.SaveChanges();
+        }
+
+
+
+    }
+}
